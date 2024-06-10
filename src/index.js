e2b404e49c4275ee68a213b9f35169cbe2ce5d87
akshay <akshaykathwate14@gmail.com>
2024-06-10 19:49:21 +0530

    cors and cookie-parser packages installed and asynchandler function created as the utility.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
   path:'./env'
 })
- 
-connectDB();
+
+
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000 )
+  {
+    console.log(`listening on port ${process.env.PORT}`); 
+  };
+}).catch((e)=>{
+  console.log("mongodb Connection failed: " + e)
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+
+const asyncHandler = (requesthandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(requesthandler(req,res,next))
+        .catch((err) =>next(err))
+    }
+}
+
+// const asyncHandler = (func) => async (req,res,next)  =>
+//     {
+//         try{
+//             await func(next, req, res);
+
+//         } catch(err){
+//             res.status(err.code || 500).json({
+//                 success: false,
+//                 messege:err.message
+//              })
+ //       }
+//   }
+
+export {asyncHandler} ;
